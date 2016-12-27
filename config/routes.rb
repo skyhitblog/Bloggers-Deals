844cdd0d06dde9faea0adf24f828ba08d025de85
Avi Jit <avijit@skyhitblog.com>
2016-12-27 23:43:26 +0530

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'pages/about'
+  get 'pages/contact'
+
   resources :deals
   root 'deals#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
